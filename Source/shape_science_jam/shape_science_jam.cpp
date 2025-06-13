e149ac44291789dcd8f22603e43e115999ca3204
Jack Blute <johnblute66@gmail.com>
2025-06-13 18:59:24 -0400

    initial commit

---
+++ Source/shape_science_jam/shape_science_jam.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "shape_science_jam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, shape_science_jam, "shape_science_jam" );
