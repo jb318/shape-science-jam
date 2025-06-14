8ff194bc433f772f59ef1ff0802e08118551dc19
Jack Blute <johnblute66@gmail.com>
2025-06-14 14:14:46 -0400

    dropping random things here to help get this project rolling

---
+++ Source/shape_science_jam/Public/CombatInterface.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CombatInterface.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHAPE_SCIENCE_JAM_API UCombatInterface : public UObject
+{
+	GENERATED_BODY()
+	
+};
