c755c3663beac53bd138b26fc89fb1b7cfaa93bd
Jack Blute <johnblute66@gmail.com>
2025-12-01 22:20:15 -0500

    removing gitignore changes

--- Plugins/SteamCorePro_5.4/Intermediate/Build/Win64/x64/UnrealEditor/Development/OnlineSubsystemSteamCore/PCH.OnlineSubsystemSteamCore.h
+++ Plugins/SteamCorePro_5.4/Intermediate/Build/Win64/x64/UnrealEditor/Development/OnlineSubsystemSteamCore/PCH.OnlineSubsystemSteamCore.h
@@ -1,3 +1,3 @@
-// PCH for F:/Unreal Projects/shape-science-jam/Plugins/SteamCorePro_5.4/Source/OnlineSubsystemSteamCore/Private/OnlineSubsystemSteamCorePrivatePCH.h
-#include "F:/Unreal Projects/shape-science-jam/Plugins/SteamCorePro_5.4/Intermediate/Build/Win64/x64/UnrealEditor/Development/OnlineSubsystemSteamCore/Definitions.h"
-#include "F:/Unreal Projects/shape-science-jam/Plugins/SteamCorePro_5.4/Source/OnlineSubsystemSteamCore/Private/OnlineSubsystemSteamCorePrivatePCH.h"
+// PCH for C:/Users/JB/Unreal Projects/shape-science-jam/Plugins/SteamCorePro_5.4/Source/OnlineSubsystemSteamCore/Private/OnlineSubsystemSteamCorePrivatePCH.h
+#include "C:/Users/JB/Unreal Projects/shape-science-jam/Plugins/SteamCorePro_5.4/Intermediate/Build/Win64/x64/UnrealEditor/Development/OnlineSubsystemSteamCore/Definitions.h"
+#include "C:/Users/JB/Unreal Projects/shape-science-jam/Plugins/SteamCorePro_5.4/Source/OnlineSubsystemSteamCore/Private/OnlineSubsystemSteamCorePrivatePCH.h"
